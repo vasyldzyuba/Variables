5a4940275d713261c618aff5541fe628558b464a
vasyldzyuba <vasyl.dzuba1@gmail.com>
2016-11-24 21:32:36 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    static byte b = 5;
+    static short sh = 8;
+    static int it = 99;
+    static long l = 789;
+
+    static float fl = 2.6f;
+    static double db = 2.6;
+
+    static boolean bool = true;
+    static boolean bool2 = false;
+
+     public static void main(String[] args){
+
+         System.out.println("byte: " + b);
+         System.out.println("short: " + sh);
+         System.out.println("int: " + it);
+         System.out.println("long: " + l);
+
+         System.out.println("float: " + fl);
+         System.out.println("double: " + db);
+         System.out.println(bool);
+         System.out.println(bool2);
+     }
+
+}
+
+
+
