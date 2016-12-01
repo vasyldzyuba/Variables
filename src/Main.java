cf6188ad4bed0724da6cb4895c9ae763d17eccfd
vasyldzyuba <vasyl.dzuba1@gmail.com>
2016-12-01 23:21:10 +0200

    math (lesson10)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
 public class Main {
 
-    static Integer in = 2;
-    static Float fl = 2.6f;
-    static Short s = 3;
-    static Double f = 7.8;
-    static String st = "27";
-
-    public static void main(String[] args) {
-        Number n = fl;
-        Number n1 = new Double(2.6);
-        Integer r1 = n.intValue();
-        Integer r2 = n1.intValue();
-        Number n3 = f;
-        int ns = n3.intValue();
-
-        System.out.println(r1);
+  public static void main(String[] args){
+  int a = 4;
+  double b = 5.4;
 
 
 
+      System.out.println(Math.cos(b) + a);
 
 
-    }
-
-
+  }
 }
 
 
