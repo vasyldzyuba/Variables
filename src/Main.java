ab38a4bede94b13e9f4e6b1f12dfa3aa370282ba
vasyldzyuba <vasyl.dzuba1@gmail.com>
2016-12-02 00:00:58 +0200

    Lesson 12

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,16 @@
 public class Main {
 
-    static boolean bool = true;
-    static int a = 5;
+  public  static  void  main(String[] args){
+  int a = 5;
+int b = 5;
 
+  boolean bool = true;
 
-    public static void main(String[] args) {
-
-        switch (6) {
-            case 5:
-                System.out.println("5");
-                break;
-
-
-            case 6:
-                System.out.println("6");
-                break;
-            case 1:
-                System.out.println("1");
-                break;
-
-        }
-    }
-
+  if(!( a == b && || bool)){
+      System.out.println("Це істина!");
+  }
+  }
 }
-
 
 
 
