22c217c7ff1f058b060a9a65d3b5b7990333697c
vasyldzyuba <vasyl.dzuba1@gmail.com>
2016-12-03 00:57:18 +0200

    Lesson 15

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        int val = calcSum(2, 9);
+        int val = calcSum(1, -1);
         System.out.println(val);
     }
 
     static int calcSum(int a, int b) {
         int sum = a + b;
-
+        if (sum == 0){
+            sum = calcSum(1,1);
+        }
         return sum;
     }
 }
